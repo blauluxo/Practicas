f2667c5983155ece0e3ce4e65122fba62965157f
blauluxo <99695777+blauluxo@users.noreply.github.com>
2023-03-30 10:57:14 +0200

    v2.2

---
+++ src/main/java/com/example/pruebados/Dtos/MedicoDto.java
@@ -0,0 +1,4 @@
+package com.example.pruebados.Dtos;
+
+public class MedicoDto {
+}
---
+++ src/main/java/com/example/pruebados/Dtos/PacienteDto.java
@@ -0,0 +1,11 @@
+package com.example.pruebados.Dtos;
+
+public class PacienteDto {
+
+    int id;
+    String nombre;
+    String apellidos;
+    String usuario;
+
+
+}
